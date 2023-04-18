34ef6798311d696931160d99bfacbfe242421c9a
fah <arpapas.f@hotmail.com>
2023-04-18 11:22:10 +0700

    comment,chart

--- routes/youtube_route.py
+++ routes/youtube_route.py
@@ -1,3 +1,4 @@
+
 # pip3 install google-api-python-client
 
 #client id = 609992228344-n2cljdiihm2mm050psieumeek19l4dud.apps.googleusercontent.com
@@ -53,4 +54,61 @@ async def get_data(artist_name: str):
     #could add likeCount ,commentCount 
     stat = video['items'][0]['statistics']['viewCount']
 
-    return artist_name,video_name,stat ,channel_name,ch_sub,ch_view
+    return artist_name,video_name,stat ,channel_name,ch_sub,ch_view,ch_videoCount
+
+@app.get("/comment_yt/{artist_name}")
+async def get_comment(artist_name: str):
+    search = youtube.search().list(
+        part='snippet,id',
+        q=artist_name,
+        maxResults='1',
+        type='video',
+        regionCode='TH',
+        videoCategoryId=categoryID).execute()
+
+    video_Id = search['items'][0]['id']['videoId']
+    video_name = search['items'][0]['snippet']['title']
+
+    # could select to show comment from video or from channel
+    # could add searchTerms to show comment with keyword e.g.'consert'
+
+    # show from video
+    result_num = 5
+    comment_top = youtube.commentThreads().list(
+        part='snippet',
+        videoId = video_Id,
+        maxResults = result_num,
+        order = 'relevance',
+        textFormat = 'plainText'
+    ).execute()
+
+    comment_result=[]
+
+    # show only comments's text
+    for i in range(result_num):
+        comment_result.append(comment_top['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'])
+    
+    return  video_name,comment_result
+    
+
+
+@app.get("/popular")
+
+def get_chart():
+
+    request = youtube.videos().list(
+        part="snippet",
+        chart="mostPopular",
+        regionCode="TH",
+        videoCategoryId="10",
+        maxResults="10"
+    )
+    response = request.execute()
+
+    # show only videos's name 
+    # could change to show other
+    chart =[]
+    for i in range(10):
+        chart.append(response['items'][i]['snippet']['title'])
+
+    return chart
